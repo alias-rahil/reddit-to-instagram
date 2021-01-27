4d64f513f7623b10fdd5fb97fa6e8b52a58dd08a
alias-rahil <rahil.kabani.4@gmail.com>
2021-01-27 07:50:22 +0530

    posting more frequently now

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import './misc/dotenv.js';
 import nodeSchedule from 'node-schedule';
-import { postSchedule, clearArraySchedule } from './misc/constants.js';
+import { postSchedule, clearArraySchedule, posts } from './misc/constants.js';
 import postInsta from './utils/postInsta.js';
-import clearPosts from './utils/clearPosts.js';
 
 nodeSchedule.scheduleJob(postSchedule, () => {
 	postInsta();
 });
 
 nodeSchedule.scheduleJob(clearArraySchedule, () => {
-	clearPosts();
+	posts.length = 0;
 });
--- src/utils/clearPosts.js
+++
@@ -1,7 +0,0 @@
-import { posts } from '../misc/constants.js';
-
-export default function clearPosts() {
-	for (let i = 0; i < posts.length; i += 1) {
-		posts.pop();
-	}
-}
