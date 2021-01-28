7755922486cd6a7395b83c3a6ee61060a50f5944
alias-rahil <rahil.kabani.4@gmail.com>
2021-01-29 04:15:01 +0530

    fix subs and tags

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import './misc/dotenv.js';
 import nodeSchedule from 'node-schedule';
 import postInsta from './utils/postInsta.js';
+import getRandomArbitrary from './utils/getRandomArbitrary.js';
 import {
 	postSchedule, clearArraySchedule, posts, maxRandomDelay,
 } from './misc/constants.js';
-
-import getRandomArbitrary from './utils/getRandomArbitrary.js';
 
 nodeSchedule.scheduleJob(postSchedule, () => {
 	setTimeout(postInsta, getRandomArbitrary(0, maxRandomDelay));
